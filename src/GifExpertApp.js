bdbd2a483c5e9f7fb32ba925821bb308229b9549
ESPERANZA JIMENEZ AVILA <3005576@insags.com>
2021-10-15 10:18:53 +0200

    remove comment lines

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -7,16 +7,11 @@ import { GifGrid } from './components/GifGrid';
 export const GifExpertApp = () => {
     const [categories, setCategories] = useState(['One Punch']);
 
-    // const handleAdd = () => {
-    //     setCategories(cat => [...cat, 'Nuevo']);
-    // }
-
     return (
         <>
             <h2>GifExpertApp</h2>
             <hr />
             <AddCategory setCategories={setCategories} />
-            {/* <button onClick={handleAdd}>Agregar</button> */}
 
             <ol>
                 {
