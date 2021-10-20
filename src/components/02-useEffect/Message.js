8afb18b0f799e4477a1f2f15eb91eba02a62a44d
ESPERANZA JIMENEZ AVILA <3005576@insags.com>
2021-10-20 17:26:18 +0200

    added hook

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,16 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function Message() {
+    const [coords, setcoords] = useState({x:0,y:0});
+    const {x,y}= coords;
+
     useEffect(() => {
-        console.log("Componente montado")
+        const mouseMove= (e)=> {
+        const coords = {
+                x:e.x,
+                y:e.y
+            };
+            setcoords(coords);
+        }
+        window.addEventListener('mousemove',mouseMove);
+        
         return () => {
-            console.log("Componente desmontado")
+            window.removeEventListener('mousemove',mouseMove);
         }
     }, [])
 
     return (
         <div>
             <h3>Eres genial!</h3>
+            <p>
+                x:{x}, y:{y}
+            </p>
         </div>
     )
 }
